6699b0b4da9ac58cedd835778c149b181aafd653
Thang <ducthang310@gmail.com>
2020-07-31 23:57:35 +0700

    Image Downloader

--- src/app/domains/landing-page/landing-page.module.ts
+++ src/app/domains/landing-page/landing-page.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { IndexComponent } from './pages/index/index.component';
+import { UiModule } from '../../shared/ui/ui.module';
 
 
 @NgModule({
   declarations: [IndexComponent],
-  imports: [
-    CommonModule,
-    LandingPageRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        LandingPageRoutingModule,
+        UiModule
+    ]
 })
 export class LandingPageModule { }
--- src/app/domains/landing-page/pages/index/index.component.ts
+++ src/app/domains/landing-page/pages/index/index.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+interface ZippedItem {
+  state: 'none' | 'processing' | 'done';
+  url?: string;
+}
+
 @Component({
   selector: 'app-landing-page-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
+  zippedItems: ZippedItem[];
 
-  constructor() { }
+  constructor() {
+    this.zippedItems = [
+      {state: 'none'}, {state: 'processing'}, {state: 'done'}
+    ];
+  }
 
   ngOnInit(): void {
   }

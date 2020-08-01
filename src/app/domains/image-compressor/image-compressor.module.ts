a8fef4e56cc931ea9ee41bfc5ea310dd2afbbf41
Thang <ducthang310@gmail.com>
2020-08-01 17:33:59 +0700

    Image compressor

--- src/app/domains/image-compressor/image-compressor.module.ts
+++ src/app/domains/image-compressor/image-compressor.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImageCompressorComponent } from './image-compressor/image-compressor.component';
+import { UiModule } from '../../shared/ui/ui.module';
 
 @NgModule({
     declarations: [ImageCompressorComponent],
     exports: [
         ImageCompressorComponent
     ],
-    imports: [
-        CommonModule
-    ]
+  imports: [
+    CommonModule,
+    UiModule
+  ]
 })
 export class ImageCompressorModule { }

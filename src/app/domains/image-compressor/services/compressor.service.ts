d35703a220c78066c7a6e361dfebe419b971dc43
Thang <ducthang310@gmail.com>
2020-08-02 16:26:56 +0700

    Service for Compressor

---
+++ src/app/domains/image-compressor/services/compressor.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ConfigService } from '../../../core/config.service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompressorService {
+
+  constructor(
+    private http: HttpClient,
+    private cfs: ConfigService,
+  ) { }
+
+  compress(data: FormData): Observable<any> {
+    return this.http.post<any>(this.cfs.getUrl(this.cfs.api.compress, null, false), data);
+  }
+}

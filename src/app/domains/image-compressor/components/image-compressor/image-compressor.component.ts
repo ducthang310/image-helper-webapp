da71bbed04402dc9f3f43a5d75a5d2209f951cf3
Thang <ducthang310@gmail.com>
2021-05-11 23:23:09 +0700

    Compress multiple images

--- src/app/domains/image-compressor/components/image-compressor/image-compressor.component.ts
+++ src/app/domains/image-compressor/components/image-compressor/image-compressor.component.ts
@@ -8,6 +8,7 @@ import { CompressorService } from '../../services/compressor.service';
   styleUrls: ['./image-compressor.component.scss']
 })
 export class ImageCompressorComponent implements OnInit {
+  zipFile: File;
 
   constructor(
     private compressorService: CompressorService,
@@ -15,7 +16,23 @@ export class ImageCompressorComponent implements OnInit {
   ) { }
 
   compress(): void {
-    //
+    const data = new FormData();
+    data.append('file', this.zipFile);
+    this.compressorService.compress(data)
+      .subscribe(res => {
+        this.downLoadFile(res, 'application/zip');
+      }, errorResponse => {
+        this.responseHelper.showErrorMessages(errorResponse);
+      });
+  }
+
+  downLoadFile(data: any, type: string): void {
+    const blob = new Blob([data], {type});
+    const url = window.URL.createObjectURL(blob);
+    const pwa = window.open(url);
+    if (!pwa || pwa.closed || typeof pwa.closed === 'undefined') {
+      alert('Please disable your Pop-up blocker and try again.');
+    }
   }
 
   ngOnInit(): void {
--- src/app/domains/image-compressor/image-compressor.module.ts
+++ src/app/domains/image-compressor/image-compressor.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImageCompressorComponent } from './components/image-compressor/image-compressor.component';
 import { UiModule } from '../../shared/ui/ui.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [ImageCompressorComponent],
     exports: [
         ImageCompressorComponent
     ],
-  imports: [
-    CommonModule,
-    UiModule
-  ]
+    imports: [
+        CommonModule,
+        UiModule,
+        FormsModule
+    ]
 })
 export class ImageCompressorModule { }
